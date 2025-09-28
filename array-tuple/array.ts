5216b610229845dcdff6645eb6ba6ef6c58f9c56
dev-kvt <devkvt@proton.me>
2025-09-28 11:55:55 +0530

    ravedeath still hirts

---
+++ array-tuple/array.ts
@@ -0,0 +1,9 @@
+let number : number[] = [1,223, 34,5  ];
+let names : string[] = ["alice" , "dev" , "archana "];
+
+let scores: Array<number> = [23, 34 , 45, 57];
+
+number.push(555);
+// this works 
+// while number.push("hello"); -> this throws the error , the type is not same cuz
+
---
+++ array-tuple/tuple.ts
@@ -0,0 +1,3 @@
+// tuples are fixed lenght arrays where each element can have different types
+let user: [ string ,name ] = ["divyansh"  , " 25"];
+let snp : [string , string ] = ["rs1234567" , " AG"];
